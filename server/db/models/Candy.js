153b0d7d2b440705bab0d238e3108e3f03d1361c
Aly Norrgard <amnorrgard@gmail.com>
2019-03-23 12:45:50 -0400

    completed backend requirements: created Candies model, seeded database, wrote route to serve up all candies (successful GET request in json format at /api/candies)

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -2,5 +2,30 @@ const Sequelize = require('sequelize');
 const db = require('../database');
 
 module.exports = db.define('candy', {
-  // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    validate: {
+      max: 10,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      'https://101clipart.com/wp-content/uploads/10/Candy%20Clipart%20Black%20And%20White%2001.png',
+  },
 });
